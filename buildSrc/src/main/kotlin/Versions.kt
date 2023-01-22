a5889b5a139f41c8fa18f74ce34c709159045b67
Joshua Soberg <j15soberg@gmail.com>
2023-01-22 12:26:17 -0500

    Update min API to 23

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 object Versions {
     object Android {
         object Sdk {
-            const val min = 21
+            const val min = 23
             const val compile = 33
             const val target = 33
         }
