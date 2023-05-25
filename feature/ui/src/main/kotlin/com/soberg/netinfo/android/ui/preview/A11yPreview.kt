37137c7f132e7922aacc67857771bc1515088613
Joshua Soberg <j15soberg@gmail.com>
2023-05-24 21:38:05 -0400

    Add an accessibility preview override that shows all of dark/light/small font/large font

---
+++ feature/ui/src/main/kotlin/com/soberg/netinfo/android/ui/preview/A11yPreview.kt
@@ -0,0 +1,15 @@
+package com.soberg.netinfo.android.ui.preview
+
+import android.content.res.Configuration
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Preview(name = "Light Mode", uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Preview(name = "Dark Mode", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview(name = "Small Font", fontScale = .5f)
+@Preview(name = "Large Font", fontScale = 2f)
+@Target(
+    AnnotationTarget.ANNOTATION_CLASS,
+    AnnotationTarget.FUNCTION,
+)
+annotation class A11yPreview
