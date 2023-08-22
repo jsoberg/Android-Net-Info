da2e76af85c5719096f2098a1a2ae751254f09f9
Joshua Soberg <j15soberg@gmail.com>
2023-08-22 18:25:57 -0400

    Add Compose metrics/reports to the compose convention plugin config

--- build-logic/src/main/kotlin/com/soberg/gradle/plugin/AndroidComposeConventionPlugin.kt
+++ build-logic/src/main/kotlin/com/soberg/gradle/plugin/AndroidComposeConventionPlugin.kt
@@ -8,11 +8,20 @@ import com.soberg.gradle.plugin.ext.testImplementation
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 
 class AndroidComposeConventionPlugin : Plugin<Project> {
 
+    private companion object {
+        private const val ComposeCompilerPluginPrepend =
+            "plugin:androidx.compose.compiler.plugins.kotlin"
+    }
+
     override fun apply(project: Project) = with(project) {
         configureCompose()
+        configureComposeMetrics()
     }
 
     private fun Project.configureCompose() {
@@ -26,6 +35,23 @@ class AndroidComposeConventionPlugin : Plugin<Project> {
             implementation(bom)
             androidTestImplementation(bom)
             testImplementation(bom)
+        }
+    }
+
+    private fun Project.configureComposeMetrics() {
+        val composeMetricsDir = layout.buildDirectory.dir("compose-metrics").get().asFile
+        val composeReportsDir = layout.buildDirectory.dir("compose-reports").get().asFile
+
+        tasks.withType<KotlinCompile>().configureEach {
+            kotlinOptions {
+                freeCompilerArgs = freeCompilerArgs + listOf(
+                    "-P",
+                    "$ComposeCompilerPluginPrepend:metricsDestination=${composeMetricsDir.absolutePath}",
+
+                    "-P",
+                    "$ComposeCompilerPluginPrepend:reportsDestination=${composeReportsDir.absolutePath}",
+                )
+            }
         }
     }
 }
