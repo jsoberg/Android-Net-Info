9991d19c0bc9def60c1d46539bee49d5e6750690
Joshua Soberg <soberg.dev@pm.me>
2024-04-25 20:51:41 -0400

    Remove deprecated method of setting system bar/colors

--- app/src/main/kotlin/com/soberg/netinfo/android/app/MainActivity.kt
+++ app/src/main/kotlin/com/soberg/netinfo/android/app/MainActivity.kt
@@ -3,8 +3,13 @@ package com.soberg.netinfo.android.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.soberg.netinfo.android.ui.core.theme.AppTheme
 import com.soberg.netinfo.android.ui.screen.NetworkInfoScreen
@@ -14,13 +19,17 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             AppTheme {
-                SystemUISetup()
-
                 Surface {
-                    Column {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .statusBarsPadding()
+                            .navigationBarsPadding()
+                    ) {
                         AppToolbar()
 
                         NetworkInfoScreen(
